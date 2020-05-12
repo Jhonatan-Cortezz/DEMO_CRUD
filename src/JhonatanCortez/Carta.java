d8d84fb1f9b0b53a0ea1f826498b79c54e069d8e
Jhonatan-Cortezz <64990363+Jhonatan-Cortezz@users.noreply.github.com>
2020-05-12 10:38:20 -0600

    Creacion de la clase carta para simular el juego de repartir cartas

---
+++ src/JhonatanCortez/Carta.java
@@ -0,0 +1,23 @@
+
+package JhonatanCortez;
+
+/**
+ *
+ * @author JHONATAN CORTEZ
+ */
+public class Carta {
+    //varibales que contienen las 2 opciones de las cartas
+    private final String cara;
+    private final String palo;
+
+    //se inicializan las variables con contructor
+    public Carta(String cara, String palo) {
+        this.cara = cara;
+        this.palo = palo;
+    }
+    
+    //se devuelve el objeto carta como un string
+    public String toString(){
+        return cara + " de " + palo;
+    }
+}

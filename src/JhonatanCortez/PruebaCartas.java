a24f3b09b635d5cf62f1660b43a731e9e342dc6c
Jhonatan-Cortezz <64990363+Jhonatan-Cortezz@users.noreply.github.com>
2020-05-12 10:53:58 -0600

    Agregue la clase para poder probar el funcionamineto del programa

---
+++ src/JhonatanCortez/PruebaCartas.java
@@ -0,0 +1,20 @@
+
+package JhonatanCortez;
+
+/**
+ *
+ * @author JHONATAN CORTEZ
+ */
+public class PruebaCartas {
+    public static void main(String[] args) {
+        PaqueteDeCartas paquete = new PaqueteDeCartas();
+        paquete.barajar();
+        
+        for (int i = 0; i <= 52; i++) {
+            System.out.printf("%-25s", paquete.repartirCart());
+            if (i % 4 == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
